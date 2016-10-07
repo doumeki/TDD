ff0045f403eaacbeb1b46aa5a45102f3ab8db8d4
DOUMEKI <doumeki@163.com>
2016-10-07 16:48:27 +0800

    this is my first init 2016å¹´10æ7æ¥

---
+++ TestOne.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#  -*- coding:utf-8 -*-
+#Author DOUMEKI
+
+from selenium import  webdriver
+import unittest
+
+#第一步，用Django创建一个项目， 命令 django-admin startproject <projectName>
+#第二步，使用manage.py，创建一个本地web服务器，命令manage.py runserver
+#第三步，类继承于testcase类，运行main方法后,可以自动运行test 方法。
+#第四步，查看创建的服务器地址，根据需要运行脚本
+class TestOne(unittest.TestCase):
+
+    def setUp(self):
+        self.startwith()
+
+    def tearDown(self):
+        print('closed')
+
+    def startwith(self):
+        print ('skakiow')
+        self.chrome= webdriver.Ie()
+        self.chrome.get('http://127.0.0.1:8000')
+
+
+    def test_can_start_a_list_and_retrivew_it_later(self):
+        self.assertIn('Django',self.chrome.title)
+        print ('this is test one')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
