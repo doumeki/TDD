444ab5562b10cf345f19e30e17e18b37a3a14a8f
DOUMEKI <doumeki@163.com>
2016-10-16 22:46:46 +0800

    'resolved'

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
+from django.http import HttpRequest
+
 
 # Create your tests here.
+class HomepageTest(TestCase):
+    def test_root_url(self):
+        found = resolve('/') #确发runserver?
+        #self.assertEqual(found.func,home_page)
+        print(found.func)
 
-class SmokeTest(TestCase):
-
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+    def test_home_page(self):
+        request = HttpRequest()
+        responese = home_page(request)
+        self.assertTrue(responese.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-DO lists</title>',responese.content)
+        self.assertTrue(responese.content.endswith(b'</html>'))
