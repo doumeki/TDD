f4990fa5ad0cd33d2f960a7b2c97a87ecd623b22
DOUMEKI <doumeki@163.com>
2016-10-23 19:10:03 +0800

    chap5 change Test

--- TestOne.py
+++ TestOne.py
@@ -37,6 +37,8 @@ class TestOne(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),'New to-do item did not appear in table')
         self.fail("Finish the test !")
+        self.fail("Great")
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
