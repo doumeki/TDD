fee27d54977613b315066ddbf50e6f8cd7b86af5
DOUMEKI <doumeki@163.com>
2016-10-24 00:54:28 +0800

    resolution not matched with rendtostring

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+import re
 
 
 # Create your tests here.
@@ -10,12 +11,10 @@ class HomepageTest(TestCase):
     def test_root_url(self):
         found = resolve('/') #确发runserver?
         self.assertEqual(found.func,home_page)
-        print(found.func)
 
     def test_home_page(self):
         request = HttpRequest()
         responese = home_page(request)
-        print (responese.content)
         self.assertTrue(responese.content.startswith(b'<html'))
         self.assertIn(b'<title>To-Do lists</title>',responese.content)
         self.assertTrue(responese.content.strip().endswith(b'</html>'))
@@ -23,9 +22,11 @@ class HomepageTest(TestCase):
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        print(repr(response.content))
-        self.assertEqual(response.content.decode(),expected_html)
+        expected_html = render_to_string('home.html',request=request)
+        rep = self.remove_csrf(response.content.decode())
+        expect_html = self.remove_csrf(expected_html)
+        self.assertEqual(rep,expect_html)
+
 
     def test_home_page_can_save_a_post_request(self):
         request = HttpRequest()
@@ -33,6 +34,12 @@ class HomepageTest(TestCase):
         request.POST['item_text'] = 'A new list item'
         response = home_page(request)
         self.assertIn("A new list item", response.content.decode())
-        expect_html = render_to_string('home.html',{'new_item_text':'A new list item'})
-        print(expect_html)
-        self.assertEqual(response.content.decode(),expect_html)
+        expect_html = render_to_string('home.html',{'new_item_text':'A new list item'},request=request)
+        rep = self.remove_csrf(response.content.decode())
+        expect_html = self.remove_csrf(expect_html)
+        self.assertEqual(rep,expect_html)
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
